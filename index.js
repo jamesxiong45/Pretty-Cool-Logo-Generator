27f9a17dd78d4a0188096f4490224c3c8857785c
James Xiong <jxiong783@gmail.com>
2023-03-02 17:13:06 -0800

    made comments in working files(index/shapes)

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+// imports inquirer and shapes file
 const inquirer = require('inquirer');
 const shapes = require('./lib/shapes');
 
+// returns the list of question inputs for one logo
 const questions = [
   {
     type: 'input',
     name: 'text',
     message: 'Enter up to three characters:',
+    // checks to see if there are 3 or less characters
     validate: function(value) {
       if (value.length > 3) {
         return 'Please enter up to three characters.';
@@ -32,10 +35,12 @@ const questions = [
   }
 ];
 
+
 inquirer.prompt(questions).then(answers => {
     const {text, textColor, shape, shapeColor} = answers;
     const svg = shapes[shape](text, textColor, shapeColor);
     const fs = require('fs');
+    // creates the logo and stores it in the examples directory
     fs.writeFile('examples/logo.svg', svg, function(err) {
       if (err) throw err;
       console.log('The logo has been created and stored in the examples directory!');
